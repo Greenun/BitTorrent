e471780b3116cd1cc4f641f74fc2744a5f437cd8
greenun <iopuy0234@gmail.com>
2020-01-30 19:58:33 +0900

    db controller modifying... need to determine usages

--- db/controller.py
+++ db/controller.py
@@ -1,10 +1,13 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.sql import select, text
 from sqlalchemy.orm import sessionmaker
-from .models import Base
-
+from sqlalchemy.exc import InvalidRequestError
+from models import Base, ValidNodes, TargetNodes, TorrentInfo
 
 # for memo
+# https://docs.sqlalchemy.org/en/13/orm/query.html
+# https://itnext.io/sqlalchemy-orm-connecting-to-postgresql-from-scratch-create-fetch-update-and-delete-a86bc81333dc
+
 # need to be env
 ROOT = 'postgres'
 PASSWORD = '0584qwqw'
@@ -13,98 +16,100 @@ DB_NAME = 'dht_database'
 
 
 def manage_session(task):
-	def session_task(self, *args, **kwargs):
-		session = sessionmaker(self.engine)()
-		result = task(self, session, *args, **kwargs)
-		session.close()
-		#return True
-	return session_task
+    def session_task(self, *args, **kwargs):
+        session = sessionmaker(self.engine)()
+        result = task(self, session, *args, **kwargs)
+        session.close()
+    # return True
+    return session_task
 
 
 class DHTDatabase(object):
-	def __init__(self, user, password, db_name, host='localhost', port=5432, debug=False):
-		self.user = user
-		self.password = password
-		self.db_name = db_name
-		self.host = host
-		self.port = port
+    def __init__(self, user, password, db_name, host='localhost', port=5432, debug=False):
+        self.user = user
+        self.password = password
+        self.db_name = db_name
+        self.host = host
+        self.port = port
 
-		self.engine = create_engine("postgresql://" + user
-			+ ":" + password
-			+ "@"+ host + ":" 
-			+ str(port) + "/" + db_name,
-			echo = debug
-		)
-		#self.session = sessionmaker(self.engine)()
+        self.engine = create_engine("postgresql://" + user
+                                    + ":" + password
+                                    + "@" + host + ":"
+                                    + str(port) + "/" + db_name,
+                                    echo=debug
+                                    )
+        # self.session = sessionmaker(self.engine)()
+        # Base.metadata.create_all(self.engine)
 
-		Base.metadata.create_all(self.engine)
+    def create_database(self, host='localhost', port=5432):
+        # create database if not exist
+        engine = create_engine("postgresql://" + self.user
+                               + ":" + self.password
+                               + "@" + host + ":"
+                               + str(port) + "/" + "postgres",
+                               echo=True
+                               )
 
-	def create_database(self, user, password, db_name, host='localhost', port=5432):
-	#create database if not exist
-		engine = create_engine("postgresql://" + user
-			+ ":" + password
-			+ "@"+ host + ":" 
-			+ str(port) + "/" + "postgres",
-			echo = True
-		)
+        conn = engine.connect()
+        db_name_query = select([text("datname")]).select_from(text("pg_database"))
+        result = conn.execute(db_name_query)
 
-		conn = engine.connect()
-		db_name_query = select([text("datname")]).select_from(text("pg_database"))
-		result = conn.execute(db_name_query)
+        if not (self.db_name,) in result.fetchall():
+            conn.execute("commit")  # end current transaction block
+            conn.execute("create database " + self.db_name)
+            conn.execute("commit")
+        conn.close()
+        engine.dispose()
 
-		if not (self.db_name,) in result.fetchall():
-			conn.execute("commit")#end current transaction block
-			conn.execute("create database "+self.db_name)
-			conn.execute("commit")
-		conn.close()
-		engine.dispose()
+        return True
 
-		return True
+    @manage_session
+    def insert(self, session, data):
+        # insert
+        # data --> model object to insert
+        if isinstance(data, list):
+            session.bulk_save_objects(data)
+        else:
+            session.add(data)
+        try:
+            session.commit()
+        except InvalidRequestError:
+            session.rollback()
+            return False
+        return True
 
-	def create_table(self):
-		# create table with metadata
-		pass
+    @manage_session
+    def select_all_valid(self, session):
+        # select
+        records = session.query(ValidNodes).all()
+        return records
 
-	@manage_session
-	def insert(self, session, data):
-		# insert
-		# data --> object to insert
-		print('insert')
+    @manage_session
+    def select_all_target(self, session):
+        records = session.query(TargetNodes).all()
+        return records
 
-	@manage_session
-	def select(self, session, data):
-		# select
-		pass
+    @manage_session
+    def select_all_info(self, session):
+        records = session.query(TorrentInfo).all()
+        return records
 
-	@manage_session
-	def update(self, session, data):
-		pass
+    @manage_session
+    def get_idle_nodes(self, session):
+        records = session.query(ValidNodes).filter(ValidNodes.using == False)
+        # for r in records:
+        #     print(r)
+        return records
 
-	@manage_session
-	def delete(self, session, data):
-		pass
+    @manage_session
+    def update(self, session, data):
+        pass
+
+    @manage_session
+    def delete(self, session, data):
+        pass
+
 
 if __name__ == "__main__":
-	#from sqlalchemy.sql import exists, select, text
-	#create_database(ROOT, PASSWORD, "dht_database")
-	ddb = DHTDatabase(ROOT, PASSWORD, DB_NAME, debug=True)
-	ddb.insert('test')
-	'''eg = create_engine('postgresql://'+ROOT+":"+PASSWORD+"@localhost:"+str(PORT)+"/"+"postgres", echo=True)
-	conn = eg.connect()
-	#conn.execute("commit")
-	t = select([text("datname")]).select_from(text("pg_database"))
-	
-	result = conn.execute(t)
-	print(dir(result))
-	print(result.context)
-	#print(result.fetchall())
-
-	#for i in result:
-	#	print(i)
-	if ('wiki',) in result.fetchall():
-		print("good")
-	print(result.next())
-	conn.execute("commit")
-	conn.close()
-	#print(eg)
-	'''
+    ddb = DHTDatabase(ROOT, PASSWORD, DB_NAME, debug=True)
+    ddb.select_all("abcd")
--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects import postgresql
 import datetime
@@ -7,29 +7,44 @@ Base = declarative_base()
 
 
 class ValidNodes(Base):
-	__tablename__ = 'valid_nodes'
+    __tablename__ = 'valid_nodes'
 
-	id = Column(Integer, primary_key=True)
-	node_id = Column(String, nullable=False, unique=True)
-	using = Column(Boolean, default=False)
-	created_time = Column(DateTime, default=datetime.datetime.utcnow)
-	#target node as foreing key
+    id = Column(Integer, primary_key=True)
+    node_id = Column(String, nullable=False, unique=True)
+    using = Column(Boolean, default=False)
+    created_time = Column(DateTime, default=datetime.datetime.utcnow)
 
-	def __repr__(self):
-		return f"<ValidNodes(id={self.id}, node_id={self.node_id}, using={self.using}, created_time={self.created_time})>"
+    # target node as foreing key
+
+    def __repr__(self):
+        return f"<ValidNodes(id={self.id}, node_id={self.node_id}, using={self.using}, " \
+               f"created_time={self.created_time})>"
 
 
 # 응답하는 노드 저장
 class TargetNodes(Base):
-	__tablename__ = 'target_nodes'
+    __tablename__ = 'target_nodes'
 
-	id = Column(Integer, primary_key=True)
-	node_id = Column(String, nullable=False, unique=True)
-	ip = Column(postgresql.INET, nullable=False)
-	port = Column(Integer, nullable=False)
-	created_time = Column(DateTime, default=datetime.datetime.utcnow)
+    id = Column(Integer, primary_key=True)
+    node_id = Column(String, nullable=False, unique=True)
+    ip = Column(postgresql.INET, nullable=False)
+    port = Column(Integer, nullable=False)
+    created_time = Column(DateTime, default=datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return f"<TargetNodes(id={self.id}, node_id={self.node_id}, ip={self.ip}, port={self.port}, " \
+               f"created_time={self.created_time}>"
 
 
 # 사용 가능한 info hash 저장
-class InfoHash(Base):
-	pass
+class TorrentInfo(Base):
+    __tablename__ = 'torrent_info'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=True)
+    info_hash = Column(postgresql.BYTEA, nullable=False)
+    created_time = Column(DateTime, default=datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return f"<TorrentInfo(id={self.id}, name={self.name}, info_hash={self.info_hash}, " \
+               f"created_time={self.created_time}>"
